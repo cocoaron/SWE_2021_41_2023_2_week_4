62b39fe74015c815692d0cafc14490317764b219
MINGYU KIM <104493794+cocoaron@users.noreply.github.com>
2023-09-20 22:57:51 +0900

    Add files via upload

--- arrange.sh
+++ arrange.sh
@@ -3,13 +3,12 @@
 DIR=$(pwd)
 FILES="$DIR/files"
 
-# loop and get each files
 for file in $FILES/*; do
 
     filename=$(basename "$file")    
     # get first letter of filename
     letter=$(echo "$filename" | grep -o '^.')
-    letter=${letter,,}
+    letter=$(echo "$letter" | tr '[:upper:]' '[:lower:]')
     
     if [ -d "$DIR/$letter" ]; then
         mv "$file" "$DIR/$letter/"
